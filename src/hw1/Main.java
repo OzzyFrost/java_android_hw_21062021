51844923c73a557c34572e0ddb2b94596d51a5e7
FF <5kla@mail.ru>
2021-06-22 00:29:06 +0500

    hw1

--- src/hw1/Main.java
+++ src/hw1/Main.java
@@ -2,6 +2,15 @@ package hw1;
 
 public class Main {
     public static void main(String[] args) {
+        printHelloWorld();
+        printHello();
+    }
 
+    public static void printHelloWorld(){
+        System.out.println("Hello World!");
+    }
+
+    public static void printHello(){
+        System.out.println("Hello !");
     }
 }
