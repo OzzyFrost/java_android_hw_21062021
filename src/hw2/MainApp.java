3b889055514ade3786edec875307089b33a69114
FF <5kla@mail.ru>
2021-06-25 00:31:33 +0500

    hw2

---
+++ src/hw2/MainApp.java
@@ -0,0 +1,11 @@
+package hw2;
+
+public class MainApp {
+    public static void main(String[] args) {
+        test1();
+    }
+
+    public static void test1() {
+        System.out.println("Hello 2");
+    }
+}
